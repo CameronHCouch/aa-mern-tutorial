d665f42ca2d1b55d12b6020e437e90dcdf040ee3
CameronHCouch <cameronhcouch@gmail.com>
2019-03-27 14:02:46 -0400

    Setup basic routing

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
+const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
@@ -9,7 +21,6 @@ mongoose
 
 app.get("/", (req, res) => res.send("Nice!"));
 
-const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`SUCCESS: Server running on port ${port}`));
 
