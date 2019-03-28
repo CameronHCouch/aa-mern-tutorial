2185c970750f7395d7cb92418137cc6604e511a9
CameronHCouch <cameronhcouch@gmail.com>
2019-03-28 17:49:54 -0400

    Add frontend actions, reducers, and utils for auth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Root from './components/root';
+import configureStore from './store/store';
+import jwt_decode from 'jwt-decode';
+import { setAuthToken } from './util/session_api_util';
+import { logout } from './actions/session_actions';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+// if a returning user has their session token stored in localStorage
+  if (localStorage.jwtToken) {
+    // sets token as common header for all axios requests
+    setAuthToken(localStorage.jwtToken);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    // decode token to obtain user's info
+    const decodedUser = jwt_decode(localStorage.jwtToken);
+    const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
+
+    store = configureStore(preloadedState);
+
+    const currentTime = Date.now() / 1000;
+
+    // if user's token has expired, log them out and redirect to login page
+    if (decodedUser.exp < currentTime) {
+      store.dispatch(logout());
+      window.location.href = '/login';
+    }
+    // this else is paired with if on line 12
+    // if this is a first-time user, start with an empty store
+  } else {
+    store = configureStore({});
+  }
+
+  const root = document.getElementById('root');
+
+  ReactDOM.render(<Root store={store} />, root);
+});
--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
 const passport = require('passport');
-
 const Tweet = require('../../models/Tweet');
 const validateTweetInput = require('../../validation/tweets');
 
 router.get('/', (req, res) => {
-  Tweet.find()
+  Tweet
+    .find()
     .sort({ date: -1 })
     .then(tweets => res.json(tweets))
     .catch(err => res.status(404).json({ notweetsfound: 'No tweets found' }));
 });
 
 router.get('/:id', (req, res) => {
-  Tweet.findById(req.params.id)
+  Tweet
+    .findById(req.params.id)
     .then(tweet => res.json(tweet))
     .catch(err =>
       res.status(404).json({ notweetfound: 'No tweet found with that ID'})
